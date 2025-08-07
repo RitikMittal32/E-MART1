63be796e86840be9450be7a72f13bba6b5774d6a
RitikMittal32 <ritikmittal8068@gmail.com>
2025-08-08 01:47:50 +0530

    third

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 // Update the base URL as needed for your environment
 // const BASE_URL = "http://localhost:4080"; // Local development URL
-const BASE_URL = "https://e-mart-1.onrender.com"; // Production URL
+const BASE_URL = "https://e-mart1.onrender.com"; // Production URL
 
 // WebSocket backend URL
 // export const BACKEND_URL = "ws://localhost:4080"; // Local WebSocket URL
-export const BACKEND_URL = "wss://e-mart-1.onrender.com"; // Production WebSocket URL
+export const BACKEND_URL = "wss://e-mart1.onrender.com"; // Production WebSocket URL
 
 // Set global defaults for Axios
 axios.defaults.baseURL = BASE_URL;
