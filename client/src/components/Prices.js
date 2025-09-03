a99cd54fe25f790afe93220c917cc1d0cde8f6ff
RitikMittal32 <ritikmittal8068@gmail.com>
2025-09-03 17:17:58 +0530

    latest update

--- client/src/components/Prices.js
+++ client/src/components/Prices.js
@@ -2,31 +2,31 @@ export const Prices = [
   {
     _id: 0,
     name: "0 to ₹199",
-    array: [0, 19],
+    array: [0, 199],
   },
   {
     _id: 1,
     name: "₹200 to ₹499",
-    array: [20, 39],
+    array: [200, 499],
   },
   {
     _id: 2,
     name: "₹500 to ₹749",
-    array: [40, 59],
+    array: [500, 749],
   },
   {
     _id: 3,
     name: "₹750 to ₹999",
-    array: [60, 79],
+    array: [750, 999],
   },
   {
     _id: 4,
     name: "₹1000 to ₹1999",
-    array: [80, 99],
+    array: [1000, 1999],
   },
   {
     _id: 5,
     name: "₹2000 or more",
-    array: [100, 9999],
+    array: [2000, 99999],
   },
 ];
