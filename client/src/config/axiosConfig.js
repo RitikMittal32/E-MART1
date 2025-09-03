3786580d02b08e351b4db37754d3c7bebc0bbb75
RitikMittal32 <ritikmittal8068@gmail.com>
2025-09-03 16:18:20 +0530

    latest update

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -7,7 +7,7 @@ const BASE_URL = "https://e-mart1.onrender.com"; // Production URL
 // export const BACKEND_URL = "wss://localhost:4080"; // Local WebSocket URL
 export const BACKEND_URL = "wss://e-mart1.onrender.com"; // Production WebSocket URL
 
-// axios.defaults.baseURL = BASE_URL;
-// axios.defaults.withCredentials = true; 
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.withCredentials = true; 
 
 export default axios;
